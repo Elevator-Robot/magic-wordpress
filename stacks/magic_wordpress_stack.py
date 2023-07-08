2e423dd4a67143b871097ebb79555ed745da31df
John Weland <john.weland@gmail.com>
2023-07-08 15:23:40 -0500

    adding minimal vpc

--- stacks/magic_wordpress_stack.py
+++ stacks/magic_wordpress_stack.py
@@ -1,11 +1,7 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_ec2 as ec2,
 )
 
 
@@ -13,12 +9,29 @@ class MagicWordpressStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self,
-            "MagicWordpressQueue",
-            visibility_timeout=Duration.seconds(300),
+        vpc = ec2.Vpc(self, 'vpc',
+            cidr = '10.0.0.0/16',
+            max_azs = 2,
+            enable_dns_hostnames = True,
+            enable_dns_support = True, 
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name = 'public-subnet',
+                    subnet_type = ec2.SubnetType.PUBLIC,
+                    cidr_mask = 24
+                ),
+                ec2.SubnetConfiguration(
+                    name = 'app-subnet',
+                    subnet_type = ec2.SubnetType.PRIVATE,
+                    cidr_mask = 24
+                ),
+                ec2.SubnetConfiguration(
+                    name = 'data-subnet',
+                    subnet_type = ec2.SubnetType.ISOLATED,
+                    cidr_mask = 24
+                )
+            ],
+            nat_gateways = 1,
         )
 
-        topic = sns.Topic(self, "MagicWordpressTopic")
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        
