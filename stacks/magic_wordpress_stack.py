21d8fae19bb705415e95ac2cd46829b4462873bf
John Weland <john.weland@gmail.com>
2023-07-21 14:46:49 -0500

    deploys

--- stacks/magic_wordpress_stack.py
+++ stacks/magic_wordpress_stack.py
@@ -4,12 +4,47 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_ecs as ecs,
     aws_resourcegroups as resourcegroups,
+    CfnParameter,
 )
 
 
 class MagicWordpressStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # Setup any Parameters
+        # tag_env_param = CfnParameter(
+        #     self,
+        #     "env-parameter",
+        #     type="String",
+        #     description="Environment name to be used for tagging",
+        #     default="dev",
+        #     allowed_values=["dev", "qa", "prod"],
+        # )
+        # tag_project_param = CfnParameter(
+        #     self,
+        #     "project-parameter",
+        #     type="String",
+        #     description="Environment name to be used for tagging",
+        #     default="MagicWordpress",
+        # )
+
+        # Setup Tags
+
+        # self.tags.set_tag("Environment", tag_env_param.value_as_string)
+        # self.tags.set_tag("Project", tag_project_param.value_as_string)
+
+        # Setup ResourceGroup for tagging and organizing resources
+        resourcegroups.CfnGroup(
+            self,
+            "resource-group",
+            name="magic-wordpress",
+            resource_query=resourcegroups.CfnGroup.ResourceQueryProperty(
+                type="CLOUDFORMATION_STACK_1_0",
+            ),
+        )
+
+        # Setup VPC
 
         vpc = ec2.Vpc(
             self,
@@ -78,13 +113,4 @@ class MagicWordpressStack(Stack):
             cluster=ecs_cluster,
             task_definition=task_definition,
             desired_count=1,
-        )
-
-        resourcegroups.CfnGroup(
-            self,
-            "resource-group",
-            name="magic-wordpress",
-            resource_query=resourcegroups.CfnGroup.ResourceQueryProperty(
-                type="CLOUDFORMATION_STACK_1_0",
-            ),
         )
